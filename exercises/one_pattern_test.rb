85a161381081dd385953ea78253ea9601a035fcb
Hugh Morrison <hmorri32@gmail.com>
2017-08-30 16:17:33 -0600

    one

--- exercises/one_pattern_test.rb
+++ exercises/one_pattern_test.rb
@@ -7,9 +7,7 @@ class OnePatternTest < Minitest::Test
   def test_one_more
     words = ["bigger", "better", "more", "improved", "advantageous"]
     count = 0
-    words.each do |word|
-      count += 1 if word == 'more'
-    end
+    words.each {|word| count += 1 if word == 'more'}
     exactly_one = count == 1
     assert exactly_one
   end
@@ -17,42 +15,39 @@ class OnePatternTest < Minitest::Test
   def test_not_even_one_ring
     ornaments = ["bracelet", "anklet", "earring"]
     count = 0
-    ornaments.each do |ornament|
-      # Your code goes here
-    end
+    ornaments.each {|ornament| count += 1 if ornament == 'ring'}
     exactly_one_ring = count == 1
     refute exactly_one_ring
   end
 
   def test_not_just_one_ring
-    skip
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
     count = 0
-    # Your code goes here
+    ornaments.each {|ornament| count += 1 if ornament == 'ring'}
     exactly_one_ring = count == 1
     refute exactly_one_ring
   end
 
   def test_one_time
-    skip
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
-    # Your code goes here
+    count = 0
+    words.each {|word| count += 1 if word == 'time'}
     exactly_one_time = count == 1
     assert exactly_one_time
   end
 
   def test_one_double_digit_number
-    skip
     numbers = [8, 2, 10, 333, 9, 101]
-    # Your code goes here
+    count = 0
+    numbers.each {|number| count += 1 if number > 9 && number < 100}
     exactly_one_double_digit = count == 1
     assert exactly_one_double_digit
   end
 
   def test_not_one_even_number
-    skip
     numbers = [3, 20, 81, 10, 391, 32]
-    # Your code goes here
+    count = 0
+    numbers.each {|number| count += 1 if number.even?}
     exactly_one_even_number = count == 1
     refute exactly_one_even_number
   end
